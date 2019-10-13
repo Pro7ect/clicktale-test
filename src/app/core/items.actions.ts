a5573846f7879421f8fe9bbe3bc246d64dc85ed1
Pro7ect <farorer@gmail.com>
2019-10-13 15:44:54 +0300

    Fixed pathing

--- src/app/core/items.actions.ts
+++ src/app/core/items.actions.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { createAction, props } from '@ngrx/store';
-import { Item } from '../features/items-list/interfaces/items-list.interface';
+import { Item } from 'app/features/items-list/interfaces/items-list.interface';
 
 export const GetItems = createAction('[Items] Get data');
 export const GetItemsSuccess = createAction('[Items] Get data Success', props<{ itemsList: Item[] }>());
